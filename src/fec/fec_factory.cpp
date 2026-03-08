52fada4111d1a9547e2b457598b01c09899373e4
German Feskov <g.feskov@yandex.ru>
2026-03-08 18:29:47 +0300

    feat(fec): enable convolutional AFF3CT codec instantiation

--- src/fec/fec_factory.cpp
+++ src/fec/fec_factory.cpp
@@ -1,3 +1,4 @@
+#include "fec/convolutional_codec_aff3ct.hpp"
 #include "fec/fec_factory.hpp"
 
 #include "fec/hamming_codec.hpp"
@@ -12,8 +13,7 @@ std::unique_ptr<IFecCodec> CreateCodec(const FecConfig& config) {
       return std::make_unique<HammingCodec>(config.hamming_r,
                                             config.hamming_extended);
     case CodecType::kConvolutionalAff3ct:
-      throw std::invalid_argument(
-          "Convolutional AFF3CT codec is not available in this build yet.");
+      return std::make_unique<ConvolutionalCodecAff3ct>(config);
     default:
       throw std::invalid_argument("Unknown codec type.");
   }
